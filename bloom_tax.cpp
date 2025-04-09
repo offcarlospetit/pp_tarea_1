2f336891ec2c1a321607143f7eafa99ae1ce4bbf
Carlos Petit <ca.alberto.p@gmail.com>
2025-04-08 22:08:12 -0400

    initial commit

---
+++ bloom_tax.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+string nivelToStr(int nivel)
+{
+    switch (nivel)
+    {
+    case 0:
+        return "Recordar";
+    case 1:
+        return "Entender";
+    case 2:
+        return "Aplicar";
+    case 3:
+        return "Analizar";
+    case 4:
+        return "Evaluar";
+    case 5:
+        return "Crear";
+    default:
+        return "Desconocido";
+    }
+}
+
+int strToNivel(const string &s)
+{
+    if (s == "Recordar")
+        return 0;
+    if (s == "Entender")
+        return 1;
+    if (s == "Aplicar")
+        return 2;
+    if (s == "Analizar")
+        return 3;
+    if (s == "Evaluar")
+        return 4;
+    return 5;
+}
